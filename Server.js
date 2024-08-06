233deca3733e42e2cca0222e5811625ff68558bb
saanubarii <hzzlnatss@gmail.com>
2024-08-07 03:45:03 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+const express = require('express');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const path = require('path');
